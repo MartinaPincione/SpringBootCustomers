b1a4aae486370cad2f58517c89e6a976ecc88e69
Martina Pincione <martinapin221@gmail.com>
2023-01-29 13:49:22 -0500

    postgresql application

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -2,41 +2,77 @@ package com.example.demo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity //tells spring that this is an entity in the database
 public class Customer {
-	String name;
-	Integer id;
-	String email;
-	LocalDate dob;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String email;
+	private LocalDate dob;
 	
 	
+	public Customer() {
+		
+	}
+
+
+	public Customer(Long id, String name, String email, LocalDate dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 	public LocalDate getDob() {
 		return dob;
 	}
+
+
 	public void setDob(LocalDate dob) {
 		this.dob = dob;
 	}
 	
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", id=" + id + ", email=" + email + ", dob=" + dob + "]";
-	}
+	
 	
 }
--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,57 +9,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.repository.CustomerRepository;
+
 
 @RestController
 public class CustomerController {
 	
-	private static Map<Integer, Customer> customers = new HashMap<>();
+	private final CustomerRepository customerRepo;
 	
-	//initialize customers mapping
 	
-	static {
-		
-		Customer nick = new Customer();
-		nick.setName("Nick"); 
-	    LocalDate d = LocalDate.of(1990, 10, 01);
-		nick.setDob(d); 
-		nick.setEmail("nick@gmail.com"); nick.setId(1234);
-		customers.put(nick.getId(), nick);
-		
-		
-		Customer sally = new Customer();
-		sally.setName("Sally");
-		LocalDate d1 = LocalDate.of(2000, 10, 01);
-		sally.setDob(d1);
-		sally.setEmail("sally@gmail.com");
-		sally.setId(9876);
-		customers.put(sally.getId(), sally);
-		
+	public CustomerController(CustomerRepository customerRepo) {
+		super();
+		this.customerRepo = customerRepo;
 	}
-	
-	
-	//utilize response entity 
-	
+
 	@GetMapping("/getCustomers")
 	public ResponseEntity<Object> getCustomers(){
-		return new ResponseEntity<>(customers.values(), HttpStatus.OK);
+		return new ResponseEntity<>(customerRepo.findAll(), HttpStatus.OK);
 	}
 	
-	@PostMapping("/postCustomer/{id}")
-	public ResponseEntity<Object> postCustomer(@PathVariable("id") Integer id, @RequestBody Customer customer){
-		customers.put(id, customer);
+	@PostMapping("/postCustomer")
+	public ResponseEntity<Object> postCustomer(@RequestBody Customer customer){
+		customerRepo.save(customer);
 		System.out.println(customer.getName());
 		return new ResponseEntity<>("Successfully posted customer", HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/deleteCustomer/{id}")
-	public ResponseEntity<Object> deleteCustomer(@PathVariable("id") Integer id){
-		customers.remove(id);
+	public ResponseEntity<Object> deleteCustomer(@PathVariable("id") Long id){
+		customerRepo.deleteById(id);
 		return new ResponseEntity<>("Customer was successfully deleted", HttpStatus.OK);
 	}
-	
-	
-	
 	
 
 }
